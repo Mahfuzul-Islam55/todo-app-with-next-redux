c4c94333f8bc4d31200f2bd2050914601ed48226
shimulsust999 <shimulsust999@gmail.com>
2023-03-20 16:39:37 +0600

    todos remaining section

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { IRootState } from "../redux/types";
+import { IInitialState } from "../redux/todos/types";
 
 const Footer = () => {
+  const todos: any = useSelector((state: IRootState) => state.todos);
+
+  const todosRemaining = todos.filter(
+    (todo: IInitialState) => !todo.completed
+  ).length;
+
+  const numberOfTodos = (todosRemaining: number) => {
+    if (todosRemaining === 0) return "No task left";
+    else if (todosRemaining === 1) return "1 task left";
+    else return `${todosRemaining} tasks left`;
+  };
+
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
-      <p>2 tasks left</p>
+      <p>{numberOfTodos(todosRemaining)}</p>
       <ul className="flex space-x-1 items-center text-xs">
         <li className="cursor-pointer font-bold">All</li>
         <li>|</li>
